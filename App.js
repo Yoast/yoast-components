47e76639750f7bf7c79b1e71c5d427b749b91edf
omarreiss <omar.reiss@gmail.com>
2016-08-10 18:41:50 +0200

    Undo change to app.js

--- App.js
+++ App.js
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Input from  './forms/Input';
-import ScreenReaderText from './a11y/ScreenReaderText';
-import ScreenReaderShortcut from './a11y/ScreenReaderShortcut';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<ScreenReaderShortcut anchor="hello">
-					Jump to main content
-				</ScreenReaderShortcut>
-				<a href="http://example.org">Demo text and some
-				<ScreenReaderText>
-				test 123
-				</ScreenReaderText></a>
-			</div>
+			<div>Demo text</div>
 		)
 	}
 };
