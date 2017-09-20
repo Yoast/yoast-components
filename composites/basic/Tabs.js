16bdb9b4b6b80ec0a157a49d38fd6253afda5582
IreneStr <irene@yoast.com>
2017-09-20 15:33:49 +0200

    Add Tabs and SubNavigation components

---
+++ composites/basic/Tabs.js
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Tab = styled.section`
+	padding: 18px 20px;
+	display: none;
+	
+	&.tab-active {
+		display: block;
+	}
+`;
+
+/**
+ * Creates a series of tabs.
+ */
+class Tabs extends React.Component {
+	/**
+	 * Constructs the class and sets its initial state.
+	 *
+	 * @param {Object} props The props to use within the component.
+	 *
+	 * @constructor
+	 */
+	constructor( props ) {
+		super( props );
+
+		this.state = { activeTab: this.props.activeTab };
+		this.getTabs();
+	}
+
+	/**
+	 * Checks whether props were changed and applies them to the state to change the currently active tab.
+	 *
+	 * @param {Object} newProps The new props to use.
+	 *
+	 * @returns {void}
+	 */
+	componentWillReceiveProps( newProps ) {
+		this.setState( { activeTab: newProps.activeTab } );
+	}
+
+	/**
+	 * Gets all the defined tabs and returns an array of ReactElements.
+	 *
+	 * @returns {Array} Array containing a Tab component for every defined item found in the props.
+	 */
+	getTabs() {
+		return this.props.items.map( ( item ) => {
+			return this.addTab( item.id, item.url, item.view );
+		} );
+	}
+
+	/**
+	 * Adds the Tab component.
+	 *
+	 * @param {string} id The unique ID for the tab.
+	 * @param {string} url The current URL to check against.
+	 * @param {ReactElement} content The content of the tab.
+	 *
+	 * @returns {ReactElement} The rendered Tab component.
+	 */
+	addTab( id, url, content ) {
+		return <Tab key={ id } className={ url === this.state.activeTab ? "tab-active" : "" }>{ content }</Tab>
+	}
+
+	/**
+	 * Renders out all the tabs.
+	 *
+	 * @returns {ReactElement} The tabs.
+	 */
+	render() {
+		return <div>{ this.getTabs() }</div>
+	}
+}
+
+Tabs.propTypes = {
+	isActive: PropTypes.bool,
+};
+
+Tabs.defaultProps = {
+	isActive: false,
+};
+
+export default Tabs;
