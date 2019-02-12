b6cb8132a6cd76808c6df5edbcb2705940b414ee
Alexander Botteram <a.botteram@gmail.com>
2019-02-12 13:25:15 +0100

    Update to styled-components v4

--- composites/Plugin/SnippetEditor/components/SnippetEditor.js
+++ composites/Plugin/SnippetEditor/components/SnippetEditor.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 // External dependencies.
 import React from "react";
 import PropTypes from "prop-types";
@@ -32,24 +33,24 @@ import colors from "../../../../style-guide/colors";
 import ErrorBoundary from "../../../basic/ErrorBoundary";
 import { getRtlStyle } from "../../../../utils/helpers/styled-components";
 
-const SnippetEditorButton = Button.extend`
+const SnippetEditorButton = styled(Button)`
 	height: 33px;
 	border: 1px solid #dbdbdb;
 	box-shadow: none;
 	font-family: Arial, Roboto-Regular, HelveticaNeue, sans-serif;
 `;
 
-const EditSnippetButton = SnippetEditorButton.extend`
-	margin: ${ getRtlStyle( "10px 0 0 4px", "10px 4px 0 0" ) };
-	fill: ${ colors.$color_grey_dark };
+const EditSnippetButton = styled(SnippetEditorButton)`
+	margin: ${getRtlStyle( "10px 0 0 4px", "10px 4px 0 0" )};
+	fill: ${colors.$color_grey_dark};
 	padding-left: 8px;
 
 	& svg {
-		${ getRtlStyle( "margin-right", "margin-left" ) }: 7px;
+		${getRtlStyle( "margin-right", "margin-left" )}: 7px;
 	}
 `;
 
-const CloseEditorButton = SnippetEditorButton.extend`
+const CloseEditorButton = styled(SnippetEditorButton)`
 	margin-top: 24px;
 `;
 
@@ -556,7 +557,7 @@ class SnippetEditor extends React.Component {
 					{ showCloseButton && <EditSnippetButton
 						onClick={ isOpen ? this.close : this.open }
 						aria-expanded={ isOpen }
-						innerRef={ this.setEditButtonRef }
+						ref={ this.setEditButtonRef }
 					>
 						<SvgIcon icon="edit" />
 						{ __( "Edit snippet", "yoast-components" ) }
